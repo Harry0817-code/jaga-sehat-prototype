b60ae62d2d78bc6cdb62993d8c9f59a111c88a0e
Harry0817-code <harrynugrohosusetyo@gmail.com>
2026-04-30 13:37:51 +0700

    check error reload page

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,9 @@ function App() {
     function fetchUserLogged() {
       setLoading(true);
 
+      console.log('check awal')
       setRole(prev => ({ ...prev, role: GetUserLogged() }));
+      console.log('check akhir')
 
       setInitializing(false);
       setLoading(false);
